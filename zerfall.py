8bb5b05c97730a4d9f88b6df17bc7f0e42754951
Andi <ax.the.dragon@googlemail.com>
2021-05-01 18:01:34 +0200

    update zerfall

--- zerfall.py
+++ zerfall.py
@@ -3,10 +3,9 @@ import nucMix
 from nucMix import printMix
 
 print("Berechnen von Zerfallsketten")
-print(nucData.H3.name)
-print(nucData.H3.hwz_years())
-print(nucData.U235)
-print(nucData.U235.hwz_years())
-print(nucData.daughters[nucData.Cs137].name)
-
+print("---- input Mix ----")
 printMix(nucMix.startMix)
+
+print("---- new Mix ----")
+nextMix = nucMix.iterateMix(nucMix.startMix)
+printMix(nextMix)
