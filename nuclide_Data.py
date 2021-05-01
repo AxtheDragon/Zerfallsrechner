806b9dabf5b0af48c32a32a2713932825a5f1639
Andi <ax.the.dragon@googlemail.com>
2021-05-01 15:57:02 +0200

    added nuclide relations

--- nuclide_Data.py
+++ nuclide_Data.py
@@ -1,5 +1,9 @@
-import nucClass
+from nucClass import nuclide
 
-H3 = nucClass.nuclide("H-3", 12.3)
-Cs137 = nucClass.nuclide("Cs-137", 30.17)
-U235 = nucClass.nuclide("U-235", 7.038*10**8)
+H3 = nuclide("H-3", 12.3)
+Cs137 = nuclide("Cs-137", 30.17)
+U235 = nuclide("U-235", 7.038*10**8)
+Ba133 = nuclide("Ba-133", 1/365)
+Li3 = nuclide("Li-3", 0)
+
+daughters = {Cs137 : Ba133, H3 : Li3}
