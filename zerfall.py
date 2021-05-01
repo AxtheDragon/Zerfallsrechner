59b91e09967f2845a01342bb9d47d7d1363eb885
Andi <ax.the.dragon@googlemail.com>
2021-05-01 17:58:36 +0200

    Revert "updated test in zerfall"
    This reverts commit 5cfce54eb33eb94ae57b7da46d15e02820439ece.

--- zerfall.py
+++ zerfall.py
@@ -1,8 +1,9 @@
 import nuclide_Data
 
-print("---- input Mix ----")
-printMix(nucMix.startMix)
-
-print("---- new Mix ----")
-nextMix = nucMix.iterateMix(nucMix.startMix)
-printMix(nextMix)
+print("Berechnen von Zerfallsketten")
+print(nuclide_Data.H3.name)
+print(nuclide_Data.H3.hwz_years())
+print()
+print(nuclide_Data.U235.name)
+print(nuclide_Data.U235.hwz_years())
+print(nuclide_Data.daughters[nuclide_Data.Cs137].name)
