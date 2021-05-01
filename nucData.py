e087002d5cef5f54d1d666c80a5685be2b83cf84
Andi <ax.the.dragon@googlemail.com>
2021-05-01 17:34:00 +0200

    implemented iterateMix function

--- nucData.py
+++ nucData.py
@@ -1,10 +1,12 @@
 from nucClass import nuclide
 
+
 H3 = nuclide("H-3", 12.3)
 Cs137 = nuclide("Cs-137", 30.17)
 U235 = nuclide("U-235", 7.038*10**8)
 Ba133 = nuclide("Ba-133", 1/365)
 Li3 = nuclide("Li-3", 0)
-Th231 = nuclide("Th-231, ", 1)
+Th231 = nuclide("Th-231", 1)
 
-daughters = {Cs137 : Ba133, H3 : Li3, U235 : Th231}
+hwz = {"H-3": 12.3, "Cs-137": 30.17}
+daughters = {str(Cs137) : str(Ba133), str(H3) : str(Li3), str(U235) : str(Th231), str(Th231) : str(H3)}
--- nucMix.py
+++ nucMix.py
@@ -4,6 +4,18 @@ def printMix(mix):
     for key,value in mix.items():
         print(key, value)
 
+def iterateMix(mix):
+    newNuclides = { }
+    for key in mix.keys():
+        daughter = nucData.daughters[key]
+        if nucData.daughters[key] in mix:
+            print(daughter, "ist schon dabei")
+        else:
+            newNuclides[daughter] = 0
+            print(daughter, "ist neu")
+    mix.update(newNuclides)    
+    return mix
 
-startMix = {nucData.U235 : 100, nucData.Cs137 : 50}
+
+startMix = {"U-235" : 100, "Cs-137" : 50, "Th-231" : 30}
 
